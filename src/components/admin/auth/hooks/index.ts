900ac41563f4091f2e5f418f2c8d79f210642484
T Savo <evilgenius@nefariousplan.com>
2025-03-30 18:51:57 -0700

    Implement enhanced TenantGuard component with permission checking #43 (#54)

--- src/components/admin/auth/hooks/index.ts
+++ src/components/admin/auth/hooks/index.ts
@@ -1,2 +1,4 @@
 export * from './useAuth';
 export { default as useAuth } from './useAuth';
+export * from './useTenantPermission';
+export { default as useTenantPermission } from './useTenantPermission';
